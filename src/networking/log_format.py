e27963d35b1a0121e9acca6cfae7939d0e7f898e
Zachareee <zacharytanhx@yahoo.com>
2026-02-10 10:57:29 +0800

    feat: added colourful logger, hide Node internals

---
+++ src/networking/log_format.py
@@ -0,0 +1,49 @@
+# Source - https://stackoverflow.com/a/71081588
+# Posted by shakfu, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-02-10, License - CC BY-SA 4.0
+
+import logging
+
+
+class CustomFormatter(logging.Formatter):
+
+    white = "\x1b[97;20m"
+    grey = "\x1b[38;20m"
+    green = "\x1b[32;20m"
+    cyan = "\x1b[36;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    fmt = "%(asctime)s - {}%(levelname)-8s{} - %(name)s:%(lineno)d - %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: fmt.format(grey, reset),
+        logging.INFO: fmt.format(green, reset),
+        logging.WARNING: fmt.format(yellow, reset),
+        logging.ERROR: fmt.format(red, reset),
+        logging.CRITICAL: fmt.format(bold_red, reset),
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt, datefmt="%H:%M:%S")
+        return formatter.format(record)
+
+
+handler = logging.StreamHandler()
+handler.setFormatter(CustomFormatter())
+logging.basicConfig(
+    level=logging.DEBUG,
+    handlers=[handler]
+)
+
+
+def create_logger(name: str, level=logging.DEBUG) -> logging.Logger:
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+    handler.setFormatter(CustomFormatter())
+
+    logger = logging.getLogger(name)
+    logging.basicConfig(level=level, handlers=[handler])
+    return logger
--- src/networking/node.py
+++ src/networking/node.py
@@ -1,12 +1,24 @@
 from namedpipe import NPopen
+from networking.log_format import CustomFormatter, create_logger
+
+logger = create_logger(__name__)
 
 
 class Node:
-    pipe: NPopen
+    MAC: str
+    IP: str
+    __MACpipe: NPopen
+    __IPpipe: NPopen
 
     def __init__(self, MAC: str, IP: str):
-        self.pipe = NPopen(name=MAC)
-        print(self.pipe)
+        self.MAC = MAC
+        self.IP = IP
+        self.__MACpipe = NPopen(name=MAC)
+        self.__IPpipe = NPopen(name=IP)
+        logger.info(self.__MACpipe)
+        logger.info(self.__IPpipe)
 
     def __del__(self):
-        self.pipe.close()
+        logger.info("closing pipes")
+        self.__MACpipe.close()
+        self.__IPpipe.close()
