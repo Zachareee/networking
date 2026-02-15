cf4da6bbfb37a0c493cde72c2ef8a8bfcbfe132e
Zachareee <zacharytanhx@yahoo.com>
2026-02-16 01:17:56 +0800

    feat: added YAML configuration, socket opening

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
-from networking.node import Node
-from networking.frames import MAC_frame
+from yaml import load, Loader
 
-print(MAC_frame.from_bytes(bytes(Node("al", 0x5).rcv_MAC_frame())))
+from networking.node import Node
+# from networking.frames import MAC_frame
+
+# print(MAC_frame.from_bytes(bytes(Node("al", 0x5).rcv_MAC_frame())))
+
+with open("config.yaml") as f:
+    data = load(f.read(), Loader)
+
+Node(data["nodes"]["N1"])
--- src/networking/node.py
+++ src/networking/node.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import override
+import socket
 
 from networking.log_format import create_logger
 from networking.frames import MAC_frame
@@ -12,6 +13,7 @@ logger = create_logger(__name__)
 class Node:
     MAC: MACaddr
     IP: int
+    __socket: socket.SocketType
 
     def rcv_MAC_frame(self) -> MAC_frame:
         frame = MAC_frame(self.MAC, "bo", "hello")
@@ -22,12 +24,17 @@ class Node:
     def send_MAC_frame(self, dst: MACaddr, data: str):
         logger.info(f"sending {data} from {self.MAC} to {dst}")
         MAC_frame(self.MAC, dst, data)
+
+    def resolve_MAC(self, dst: MACaddr):
         pass
 
     @override
-    def __init__(self, MAC: MACaddr, IP: int):
-        self.MAC = MAC
-        self.IP = IP
+    def __init__(self, node_config: dict):
+        self.MAC = node_config["MAC"]
+        self.IP = node_config["IP"]
+        self.__socket = socket.create_server(
+            ("127.0.0.1", node_config["port"]))
 
     def __del__(self):
+        self.__socket.close()
         logger.info("closing node")
